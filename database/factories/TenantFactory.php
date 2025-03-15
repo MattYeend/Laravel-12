a87318d2ac9b8fbe35925b28333689f141d0b65d
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-15 12:30:38 +0000

    New Tenant Factory

---
+++ database/factories/TenantFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tenant>
+ */
+class TenantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company,
+            'domain' => $this->faker->unique()->domainName,
+            'database' => $this->faker->boolean ? $this->faker->unique()->word : null,
+        ];
+    }
+}
