dee498e51046385d246ccf2deef7a01296187fc4
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-15 11:47:08 +0000

    Created new tenant model

---
+++ app/Models/Tenant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Multitenancy\Models\Tenant as BaseTenant;
+
+class Tenant extends BaseTenant
+{
+    protected $fillable = ['name', 'domain'];
+    protected $guarded = [];
+}
