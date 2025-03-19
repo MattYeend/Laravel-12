b9dbe264b37f29650db9681e0850ee89471f6a75
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-19 08:05:34 +0000

    #12 New Welcome language file

---
+++ lang/en/welcome.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Welcome Page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used on the welcome page for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'title' => 'Welcome to Our Application',
+    'subtitle' => 'Your journey to excellence begins here.',
+    'login' => 'Login',
+    'register' => 'Register',
+    'dashboard' => 'Go to Dashboard',
+    'logout' => 'Logout',
+    'language' => 'Language',
+    'home' => 'Home',
+    'contact' => 'Contact Us',
+    'about' => 'About Us',
+    'features' => 'Features',
+    'welcome_message' => 'We are glad to have you here! Explore and enjoy our services.',
+
+];
